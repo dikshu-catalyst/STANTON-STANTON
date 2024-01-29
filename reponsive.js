63941a5697c03eeb5e009d4d339d71f58c645276
dikshu-catalyst <156075007+dikshu-catalyst@users.noreply.github.com>
2024-01-29 17:42:47 +0530

    Add files via upload

---
+++ reponsive.js
@@ -0,0 +1,25 @@
+// $(".controls-line").click(function () {
+//     $(".menu, .cross").toggle("");
+//   });
+
+// $(".controls-line").click(function () {
+//     $(".menu").toggle();
+//     $(".cross").toggle();
+// });
+
+
+
+// $(document).ready(function () {
+//     $(".controls-line").click(function () {
+//         $(".menu").toggle();
+//         $(".cross").toggle(active);
+//     });
+// });
+
+$(".controls-line").click(function () {
+    $(".menu").toggle();
+    $(".cross").toggleClass("active");
+  });
+  
+
+  
